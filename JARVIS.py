987739a7b3f68e52066c57df78ac92189eb19576
Sa2sha3 <oleksandr.taranenko2208@gmail.com>
2023-06-30 19:42:57 +0300

    add voices

--- JARVIS.py
+++ JARVIS.py
@@ -1,4 +1,6 @@
 import os
+import webbrowser
+
 import speech_recognition as sr
 
 #
@@ -18,5 +20,25 @@ talk("Hello")
 
 def command():
     r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Say")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
+
+    task = r.recognize_google(audio, language="en-US").lower()
+    print("You: " + task)
+
+    return task
+
+def make_something(ar_task):
+    if ("open" and "site") in ar_task:
+        talk("ok")
+        url = "https://ituniver.com"
+        webbrowser.open(url)
+
+
+make_something(command())
 
 command()
